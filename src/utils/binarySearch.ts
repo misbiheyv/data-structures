de5c2f8e8bb17a93c92b1baad8b33eb28cf21722
misbiheyv <mishagorkovenko1103@gmail.com>
2022-10-15 20:53:14 +0300

    Реализовал бинарный поиск

---
+++ src/utils/binarySearch.ts
@@ -0,0 +1,19 @@
+import { CanUndef } from "../interface";
+
+export function binarySearch(target: any, arr: any[], left?: number, right?: number): CanUndef<number> {
+    const r = right ?? arr.length;
+    const l = left ?? 0;
+
+    const pivot = Math.floor((r + l)/2);
+    const el = arr[pivot];
+
+    if (target === el) return pivot;
+
+    if (l === r) return undefined;
+
+    if (target < el) {
+        return binarySearch(target, arr, l, pivot);
+    } else {
+        return binarySearch(target, arr, pivot + 1, r);
+    }
+}
